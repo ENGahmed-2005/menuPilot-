ab74b0ee2cb93e09ff0e7edc3afdf4097bbb58f4
عمار يحيى عمر العرعير <aa409@smail.ucas.edu.ps>
2026-09-09 14:25:27 +0300

    Add restaurant model

---
+++ backend/app/Models/Restaurant.php
@@ -0,0 +1,4 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+class Restaurant extends Model { protected $fillable=['name']; public function owners(){return $this->hasMany(Owner::class);} public function categories(){return $this->hasMany(Category::class);} public function products(){return $this->hasMany(Product::class);} public function tables(){return $this->hasMany(Table::class);} }
