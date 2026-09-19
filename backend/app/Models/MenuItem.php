ee415b3aa4a87c60b9325cef156a3d0c810f4eaa
ENG Ahmed Alkahlout <ahmedkahlout258@gmail.com>
2026-09-19 14:30:49 +0300

    Use soft deletes for menu items

--- backend/app/Models/MenuItem.php
+++ backend/app/Models/MenuItem.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MenuItem extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = ['user_id', 'name', 'price', 'category', 'description', 'image_url', 'is_available'];
 
